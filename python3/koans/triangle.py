9621380aefe8a40cdcc49194ed47d5cd656efea4
tanah <tana@cbmperdoma.es>
2020-07-18 21:34:57 +0100

    Changes up to 46% completion

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,17 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    #pass
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError
+    elif a+b < c or b+c < a or c+a < b:
+        raise TriangleError
+    if a == b == c:
+        return 'equilateral'
+    elif a==b or b==c or a==c:
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
